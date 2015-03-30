595fb2095f6db8fdde38da403d8c6c7c035d3c67
Daniel Ourada <daniel.ourada@gmail.com>
2015-03-30 12:20:00 -0700

    updated to integration

--- HostedCheckout.MVC.CSharp/HostedCheckout.MVC.CSharp/Controllers/HomeController.cs
+++ HostedCheckout.MVC.CSharp/HostedCheckout.MVC.CSharp/Controllers/HomeController.cs
@@ -15,7 +15,33 @@ namespace HostedCheckout.MVC.CSharp.Controllers
 
         public ActionResult Buy()
         {
-            ViewBag.Message = "Your application description page.";
+            HCService.HCServiceSoapClient client = new HCService.HCServiceSoapClient();
+            HCService.InitPaymentRequest req = new HCService.InitPaymentRequest();
+            req.MerchantID = "013163015566916";
+            req.Password = "ypBj@f@zt3fJRX,k";
+            req.Invoice = "1234";
+            req.TotalAmount = 1.23;
+            req.TaxAmount = 0;
+            req.TranType = "Sale";
+            req.Frequency = "OneTime";
+            req.Memo = "dano test";
+            req.ProcessCompleteUrl = "http://localhost:51619/Home/Complete";
+            req.ReturnUrl = "http://localhost:51619/Home/Return";
+            req.OperatorID = "test";
+            var resp = client.InitializePayment(req);
+
+            ViewBag.URL = "https://hc.mercurydev.net/CheckoutIFrame.aspx?ReturnMethod=get&pid=" + resp.PaymentID;
+            return View();
+        }
+
+        public ActionResult Complete(string PaymentID, string ReturnCode, string ReturnMessage)
+        {
+            HCService.HCServiceSoapClient client = new HCService.HCServiceSoapClient();
+            HCService.PaymentInfoRequest req = new HCService.PaymentInfoRequest();
+            req.MerchantID = "013163015566916";
+            req.Password = "ypBj@f@zt3fJRX,k";
+            req.PaymentID = PaymentID;
+            var resp = client.VerifyPayment(req);
 
             return View();
         }
