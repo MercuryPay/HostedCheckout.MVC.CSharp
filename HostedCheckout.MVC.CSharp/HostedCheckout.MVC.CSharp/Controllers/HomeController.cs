9ad4456adbe12e52f77437a8a13da4dea5e43c2e
Andrew Green Harris <agharris73@gmail.com>
2015-03-31 16:37:13 -0600

    css working

--- HostedCheckout.MVC.CSharp/HostedCheckout.MVC.CSharp/Controllers/HomeController.cs
+++ HostedCheckout.MVC.CSharp/HostedCheckout.MVC.CSharp/Controllers/HomeController.cs
@@ -13,38 +13,12 @@ namespace HostedCheckout.MVC.CSharp.Controllers
             return View();
         }
 
-        public ActionResult CSS ()
-        {
-            HCService.CssUploadRequest request = new HCService.CssUploadRequest();
-            request.MerchantID = "013163015566916";
-            request.Password = "KR568r@1spyC13,T";
-            request.Css = ".divTotalDefault{color: #000000;background-color: red;}";
-
-            //Call the web service to initialize the UploadCSS request.
-            HCService.HCServiceSoapClient client = new HCService.HCServiceSoapClient();
-            HCService.CssAdminResponse response = new HCService.CssAdminResponse();
-                response = client.UploadCSS(request);
-                if (response != null)
-                {
-                    var blah = response.ResponseCode.ToString();
-                    var blah2 = HttpUtility.HtmlDecode(response.Message);
-                }
-
-                else
-                {
-                    //something seriously wrong. probably couldn't connect to the web service at all
-                    var blah3 = "Response from UploadCss was Null";
-
-                }
-                return View();
-        }
-
         public ActionResult Buy()
         {
             HCService.HCServiceSoapClient client = new HCService.HCServiceSoapClient();
             HCService.InitPaymentRequest req = new HCService.InitPaymentRequest();
-            req.MerchantID = "013163015566916";
-            req.Password = "ypBj@f@zt3fJRX,k";
+            req.MerchantID = "912127036979183";
+            req.Password = "e%MH0HJs3$3wk+ob";
             req.Invoice = "1234";
             req.TotalAmount = 1.23;
             req.TaxAmount = 0;
@@ -54,6 +28,7 @@ namespace HostedCheckout.MVC.CSharp.Controllers
             req.ProcessCompleteUrl = "http://localhost:51619/Home/Complete";
             req.ReturnUrl = "http://localhost:51619/Home/Return";
             req.OperatorID = "test";
+            req.DisplayStyle = "custom";
             var resp = client.InitializePayment(req);
 
             ViewBag.URL = "https://hc.mercurydev.net/CheckoutIFrame.aspx?ReturnMethod=get&pid=" + resp.PaymentID;
@@ -64,12 +39,39 @@ namespace HostedCheckout.MVC.CSharp.Controllers
         {
             HCService.HCServiceSoapClient client = new HCService.HCServiceSoapClient();
             HCService.PaymentInfoRequest req = new HCService.PaymentInfoRequest();
-            req.MerchantID = "013163015566916";
-            req.Password = "ypBj@f@zt3fJRX,k";
+            req.MerchantID = "912127036979183";
+            req.Password = "e%MH0HJs3$3wk+ob";
             req.PaymentID = PaymentID;
             var resp = client.VerifyPayment(req);
 
             return View();
         }
+
+        public ActionResult CSS()
+        {
+            
+HCService.CssUploadRequest request = new HCService.CssUploadRequest();
+            request.MerchantID = "912127036979183";
+            request.Password = "e%MH0HJs3$3wk+ob";
+            request.Css = ".btnDefaultIFrame {color: #fff; background-color: #f7901e; border-color: transparent;}";
+
+
+            HCService.HCServiceSoapClient client = new HCService.HCServiceSoapClient();
+            HCService.CssAdminResponse response = new HCService.CssAdminResponse();
+            response = client.UploadCSS(request);
+            if (response != null)
+            {
+                var blah = response.ResponseCode.ToString();
+                var blah2 = HttpUtility.HtmlDecode(response.Message);
+            }
+
+            else
+            {
+                var blah3 = "Response from UploadCss was Null";
+
+            }
+            return View();
+        }
+
     }
 }
